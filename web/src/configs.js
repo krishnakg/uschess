fd0e3f354bf47acd2c16f946a7b47c795d335513
Krishna Gopalakrishnan <krishnakg@gmail.com>
2018-03-10 13:48:00 -0800

    Install axis for the project and adjust api urls in configs

--- web/src/configs.js
+++ web/src/configs.js
@@ -1,11 +1,11 @@
-const apiServerUrl = "http://localhost:8080/";
+const apiServerUrl = "http://localhost:8080";
 const Configs = {
-  playerInfoUrl : apiServerUrl + "players/",
-  playerEventsUrl : apiServerUrl + "events?uscf_id=",
-  playerSearchUrl : apiServerUrl + "playersearch/",
-  tournamentInfoUrl : apiServerUrl + "tournaments/",
-  sectionResultUrl : apiServerUrl + "sections/",
-  sectionGamesUrl : apiServerUrl + "games/"
+  playerInfoUrl : apiServerUrl + "/players/",
+  playerEventsUrl : apiServerUrl + "/events?uscf_id=",
+  playerSearchUrl : apiServerUrl + "/playersearch/",
+  tournamentInfoUrl : apiServerUrl + "/tournaments/",
+  sectionResultUrl : apiServerUrl + "/sections/",
+  sectionGamesUrl : apiServerUrl + "/games/"
 };
 
 export default Configs;
